5b470f2aa24791aea1c89674ea2a96fce1e8d995
tsekatm <tebogo.tseka@gmail.com>
2023-09-04 03:36:16 +0200

    I have just completed the operator precedence lesson

---
+++ fundamentals/11_operators/OperatorPrecedence.java
@@ -0,0 +1,9 @@
+public class OperatorPrecedence{
+  public static void main(String [] args){
+    int a =10;
+    int b =4;
+    int c =6;
+    System.out.println(a+b/c++);
+  }
+}
+
---
+++ fundamentals/11_operators/OperatorPrecedence2.java
@@ -0,0 +1,8 @@
+public class OperatorPrecedence2{
+  public static void main(String [] args){
+    int a =10;
+    int b =5;
+    int c =6;
+    System.out.println(b%c);
+  }
+}
