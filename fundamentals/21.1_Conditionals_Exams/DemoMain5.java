5c9ae170af51b2bb454c7743e6718516771badd5
tsekatm <tebogo.tseka@gmail.com>
2023-10-07 11:51:54 +0200

    I am doing the post mortem for the conditionals exams

---
+++ fundamentals/21.1_Conditionals_Exams/DemoMain5.java
@@ -0,0 +1,29 @@
+public class DemoMain5 {
+
+	public static void main(String[] args) {
+		
+		double discount;
+		char code = 'C';
+
+		switch (code)
+		{
+
+		case 'A':
+			discount = 0.0;
+			System.out.println(discount);
+
+		case 'B':
+
+			discount = 0.1;
+			System.out.println(discount);
+
+		case 'C':
+			discount = 0.2;
+			System.out.println(discount);
+
+		default:
+			discount = 0.3;
+			System.out.println(discount);
+		}
+	}
+}
